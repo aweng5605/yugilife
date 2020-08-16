91c56772c317ed7c646d9609c76d69244c9eeb76
alixsep <alixsep@outlook.com>
2020-08-16 06:46:24 +0430

    Updates

---
+++ precache-manifest.fcb810ed8015c5f826d63445a7169588.js
@@ -0,0 +1,318 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20a22dab478f8afd4651027f002ae6f8",
+    "url": "/yugilife/index.html"
+  },
+  {
+    "revision": "44e2cd470c2740d44b04",
+    "url": "/yugilife/static/css/main.ed8a188b.chunk.css"
+  },
+  {
+    "revision": "163822a187f6b3426c4d",
+    "url": "/yugilife/static/js/2.c1beba74.chunk.js"
+  },
+  {
+    "revision": "f8a308b7fc55ee590675d344f7edc451",
+    "url": "/yugilife/static/js/2.c1beba74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44e2cd470c2740d44b04",
+    "url": "/yugilife/static/js/main.ec817d24.chunk.js"
+  },
+  {
+    "revision": "fbf46fb6db77ddf65b65",
+    "url": "/yugilife/static/js/runtime-main.82ba9936.js"
+  },
+  {
+    "revision": "b7db533d598811793b1bb6278933dacf",
+    "url": "/yugilife/static/media/CL1.b7db533d.svg"
+  },
+  {
+    "revision": "0b68a4e4008e64104c8182d1917400cc",
+    "url": "/yugilife/static/media/CL2.0b68a4e4.svg"
+  },
+  {
+    "revision": "84501c93ff102d5d0607394257c4aaea",
+    "url": "/yugilife/static/media/CL3.84501c93.svg"
+  },
+  {
+    "revision": "f55c4e2f7c9ad714765e2d4ebd676185",
+    "url": "/yugilife/static/media/CL4.f55c4e2f.svg"
+  },
+  {
+    "revision": "4e28c200785e870256b45d8f9d2cb28d",
+    "url": "/yugilife/static/media/CL5.4e28c200.svg"
+  },
+  {
+    "revision": "2abcfda47b9813a7fed24352d467834e",
+    "url": "/yugilife/static/media/CL6.2abcfda4.svg"
+  },
+  {
+    "revision": "858d9a2149b4d49314cebb8584757981",
+    "url": "/yugilife/static/media/CL7.858d9a21.svg"
+  },
+  {
+    "revision": "3ad96293ba6af0536cb52d7ef7122a8b",
+    "url": "/yugilife/static/media/CL8.3ad96293.svg"
+  },
+  {
+    "revision": "269129fe30f4e364420292ae78d38ddb",
+    "url": "/yugilife/static/media/CL9.269129fe.svg"
+  },
+  {
+    "revision": "938153f6ae5f0e6ec2bee82ffbdda5ca",
+    "url": "/yugilife/static/media/Chevron_down_font_awesome.938153f6.svg"
+  },
+  {
+    "revision": "5bde3fb0b088731a780422597a9b7517",
+    "url": "/yugilife/static/media/Effect.5bde3fb0.png"
+  },
+  {
+    "revision": "ad4a353989c5f47a6ad4ec67e687f7cf",
+    "url": "/yugilife/static/media/Effect.ad4a3539.png"
+  },
+  {
+    "revision": "5b3d6deed3d4d0c6f13b6afcac97cf5c",
+    "url": "/yugilife/static/media/Fusion.5b3d6dee.png"
+  },
+  {
+    "revision": "6ee4f3d8806136900996d4a686a0243c",
+    "url": "/yugilife/static/media/Fusion.6ee4f3d8.png"
+  },
+  {
+    "revision": "d76b8796b46e565a9c2460da3b8e5d4c",
+    "url": "/yugilife/static/media/Link.d76b8796.png"
+  },
+  {
+    "revision": "c7c5de58429f48f7bb80f7f42cab0aea",
+    "url": "/yugilife/static/media/Normal.c7c5de58.png"
+  },
+  {
+    "revision": "ed1556fdde2dc64e2538eac972be5d7b",
+    "url": "/yugilife/static/media/Normal.ed1556fd.png"
+  },
+  {
+    "revision": "56ac8ebe3120ebe668f99c6fbeb1921e",
+    "url": "/yugilife/static/media/PL.56ac8ebe.png"
+  },
+  {
+    "revision": "312bbbeb3dc4dfd437d2f753812aa691",
+    "url": "/yugilife/static/media/PM.312bbbeb.png"
+  },
+  {
+    "revision": "4080564884cc5fc224265adfd71dfad9",
+    "url": "/yugilife/static/media/PS.40805648.png"
+  },
+  {
+    "revision": "4f54b01d6e429297307a90823aa8f28b",
+    "url": "/yugilife/static/media/Ritual.4f54b01d.png"
+  },
+  {
+    "revision": "2b41142f2f05675c9fdc8fdfbab4c9da",
+    "url": "/yugilife/static/media/Spell.2b41142f.png"
+  },
+  {
+    "revision": "842305ec305a5dbb4b6f655819d3e5f2",
+    "url": "/yugilife/static/media/Spell.842305ec.png"
+  },
+  {
+    "revision": "1bbaf9671e49c85384760e410620d087",
+    "url": "/yugilife/static/media/StoneSerifITC-MediumItalic.1bbaf967.ttf"
+  },
+  {
+    "revision": "5670b5426ad0749a7c669477262f0b7b",
+    "url": "/yugilife/static/media/StoneSerifITC-MediumItalic.5670b542.woff"
+  },
+  {
+    "revision": "5c13b417b0063f16de2943bc24a9013c",
+    "url": "/yugilife/static/media/StoneSerifITC-MediumItalic.5c13b417.svg"
+  },
+  {
+    "revision": "9574f68e5f66d5f1215dfd22b432716b",
+    "url": "/yugilife/static/media/StoneSerifITC-MediumItalic.9574f68e.woff2"
+  },
+  {
+    "revision": "79daed63e11d1a497c5d00dc4dbdf96b",
+    "url": "/yugilife/static/media/StoneSerifStd-Bold.79daed63.svg"
+  },
+  {
+    "revision": "8aaa4559a0c791cc25b820fe3cea6017",
+    "url": "/yugilife/static/media/StoneSerifStd-Bold.8aaa4559.ttf"
+  },
+  {
+    "revision": "b12e2662afde6a0e526be2026b89b2db",
+    "url": "/yugilife/static/media/StoneSerifStd-Bold.b12e2662.woff2"
+  },
+  {
+    "revision": "b6cd19682014e6435c09c7e0281d80a1",
+    "url": "/yugilife/static/media/StoneSerifStd-Bold.b6cd1968.woff"
+  },
+  {
+    "revision": "0cc7f0197f527945c979496f45616a22",
+    "url": "/yugilife/static/media/StoneSerifStd-Medium.0cc7f019.woff"
+  },
+  {
+    "revision": "44fbff96b56be5592961e1f10af7dc6f",
+    "url": "/yugilife/static/media/StoneSerifStd-Medium.44fbff96.woff2"
+  },
+  {
+    "revision": "a3eb192f08a245ee82be3f755ff37dcd",
+    "url": "/yugilife/static/media/StoneSerifStd-Medium.a3eb192f.ttf"
+  },
+  {
+    "revision": "ad17a97ec2ff7bcf3a57b0ceb5a222ae",
+    "url": "/yugilife/static/media/StoneSerifStd-Medium.ad17a97e.svg"
+  },
+  {
+    "revision": "0bdbfdb57b2059b1b1f64a3e41016eda",
+    "url": "/yugilife/static/media/Synchro.0bdbfdb5.png"
+  },
+  {
+    "revision": "823717402fbfc08b0520f39417082566",
+    "url": "/yugilife/static/media/Synchro.82371740.png"
+  },
+  {
+    "revision": "958f0e35532c8793dd6b66abc7840b84",
+    "url": "/yugilife/static/media/TokenI.958f0e35.png"
+  },
+  {
+    "revision": "45caad559ae7d02d2bcf0d9bcc4058a4",
+    "url": "/yugilife/static/media/TokenII.45caad55.png"
+  },
+  {
+    "revision": "1785d6815f2479d0bdc0f7c125bd275f",
+    "url": "/yugilife/static/media/Trap.1785d681.png"
+  },
+  {
+    "revision": "d9fc6d050b148c9bc77965fe001b80c4",
+    "url": "/yugilife/static/media/WL-512.d9fc6d05.svg"
+  },
+  {
+    "revision": "c40fbc71ece251c30eddb3f475ec0130",
+    "url": "/yugilife/static/media/Water.c40fbc71.png"
+  },
+  {
+    "revision": "e7ec054de44aa6e2f78344501af53375",
+    "url": "/yugilife/static/media/Wind.e7ec054d.png"
+  },
+  {
+    "revision": "1eea46f6bd0bcae0e4aee3795fa67222",
+    "url": "/yugilife/static/media/XYZ.1eea46f6.png"
+  },
+  {
+    "revision": "75fbe4cbc3a78cbee80a5031e6a85421",
+    "url": "/yugilife/static/media/XYZ.75fbe4cb.png"
+  },
+  {
+    "revision": "6524a4d439b90b7fc0a7ebab7cf8f46c",
+    "url": "/yugilife/static/media/at.6524a4d4.svg"
+  },
+  {
+    "revision": "d58ecc0d30e326639dfe8570d36827a4",
+    "url": "/yugilife/static/media/backup.d58ecc0d.svg"
+  },
+  {
+    "revision": "cd7a52ed5d68811838d6d17b3c2eaa90",
+    "url": "/yugilife/static/media/build.cd7a52ed.svg"
+  },
+  {
+    "revision": "95320457b7755a802c14ad4a7bde666e",
+    "url": "/yugilife/static/media/cancel.95320457.svg"
+  },
+  {
+    "revision": "4e3eddd16c1d8af91d5b314cf7eb3971",
+    "url": "/yugilife/static/media/correct.4e3eddd1.svg"
+  },
+  {
+    "revision": "f1c802c4db7bdf7295edaf81aeda87fe",
+    "url": "/yugilife/static/media/download.f1c802c4.svg"
+  },
+  {
+    "revision": "a8cc3c59d12accd2ed8fd1825307d90d",
+    "url": "/yugilife/static/media/file.a8cc3c59.svg"
+  },
+  {
+    "revision": "f7965e11759ba78717d4c1bcbc05ea13",
+    "url": "/yugilife/static/media/free.f7965e11.svg"
+  },
+  {
+    "revision": "18d2d37766a00e7b4177cb4109cea019",
+    "url": "/yugilife/static/media/high-quality.18d2d377.svg"
+  },
+  {
+    "revision": "633ccd515ca0288b06ba3f815ed95289",
+    "url": "/yugilife/static/media/info.633ccd51.svg"
+  },
+  {
+    "revision": "090168e991e8a0b2da78fa0e9c638e77",
+    "url": "/yugilife/static/media/matrix_bold_small_caps-webfont.090168e9.eot"
+  },
+  {
+    "revision": "4842a53b7e7e1842506b1583a2ff4847",
+    "url": "/yugilife/static/media/matrix_bold_small_caps-webfont.4842a53b.ttf"
+  },
+  {
+    "revision": "4b2c3fba4d5210c63c18939bebe0f4fc",
+    "url": "/yugilife/static/media/matrix_bold_small_caps-webfont.4b2c3fba.svg"
+  },
+  {
+    "revision": "536daed7f8e48bf29787c791d36113f1",
+    "url": "/yugilife/static/media/matrix_bold_small_caps-webfont.536daed7.woff"
+  },
+  {
+    "revision": "b7a967a800b2a401c404543ecb92edfa",
+    "url": "/yugilife/static/media/matrix_bold_small_caps-webfont.b7a967a8.woff2"
+  },
+  {
+    "revision": "0a2106951ef122234d11bf1e90cce0bd",
+    "url": "/yugilife/static/media/matrix_book-webfont.0a210695.svg"
+  },
+  {
+    "revision": "3ef233030fefa50a1d9d38019042b4ba",
+    "url": "/yugilife/static/media/matrix_book-webfont.3ef23303.ttf"
+  },
+  {
+    "revision": "695e4c5f2d3a457af5624db81a09ea74",
+    "url": "/yugilife/static/media/matrix_book-webfont.695e4c5f.woff"
+  },
+  {
+    "revision": "e6f024fdb0016a79e037655e98972f26",
+    "url": "/yugilife/static/media/matrix_book-webfont.e6f024fd.woff2"
+  },
+  {
+    "revision": "ef25dd49b560534405b87ea101e5e91d",
+    "url": "/yugilife/static/media/matrix_book-webfont.ef25dd49.eot"
+  },
+  {
+    "revision": "2716965a0a2a450f81c0c95438a4c968",
+    "url": "/yugilife/static/media/matrix_regular_small_caps-webfont.2716965a.eot"
+  },
+  {
+    "revision": "54a1ab7da9f4a0a8f1aeee7654aa6c7e",
+    "url": "/yugilife/static/media/matrix_regular_small_caps-webfont.54a1ab7d.ttf"
+  },
+  {
+    "revision": "785ca1d02747c9039a23e576adc8e2bb",
+    "url": "/yugilife/static/media/matrix_regular_small_caps-webfont.785ca1d0.woff2"
+  },
+  {
+    "revision": "a00f845902417b08b6481d14c92a3f9a",
+    "url": "/yugilife/static/media/matrix_regular_small_caps-webfont.a00f8459.woff"
+  },
+  {
+    "revision": "bb91f01137533b61b7d8cc7ca9d642cf",
+    "url": "/yugilife/static/media/matrix_regular_small_caps-webfont.bb91f011.svg"
+  },
+  {
+    "revision": "6630c2ff41abc4944a37ab50d4d06dbf",
+    "url": "/yugilife/static/media/photoshop.6630c2ff.svg"
+  },
+  {
+    "revision": "c3b0d6c06ed1fe3399ca55998c280224",
+    "url": "/yugilife/static/media/play.c3b0d6c0.svg"
+  },
+  {
+    "revision": "efc6d53194a8212dacfdbce86ab457a6",
+    "url": "/yugilife/static/media/speed.efc6d531.svg"
+  }
+]);
